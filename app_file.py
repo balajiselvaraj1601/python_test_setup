819ee3ffc530d797ddca61c5d1c1024ca1bff1d2
Balaji <balaji.selvaraj@astrazeneca.com>
2023-02-07 12:01:10 +0530

    dockerfile

---
+++ app_file.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+X, Y = np.arange(10).reshape((5, 2)), range(5)
+
+print("----------------------------------------------------------")
+print(f"Value of X:  {X}")
+print(f"Value of Y:  {Y}")

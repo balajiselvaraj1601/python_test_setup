1533566f484a0bf0434a78d5dc72f124238d52b4
Balaji <balaji.selvaraj@astrazeneca.com>
2023-02-07 16:16:03 +0530

    updated apps

--- app_file.py
+++ app_file.py
@@ -5,3 +5,6 @@ X, Y = np.arange(10).reshape((5, 2)), range(5)
 print("----------------------------------------------------------")
 print(f"Value of X:  {X}")
 print(f"Value of Y:  {Y}")
+
+for i in range(0,10):
+    print(f"--------> {i}")

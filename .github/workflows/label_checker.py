35714e81287426f3860bad5b88f189655ec4af2d
Balaji Selvaraj <30400730+balajiselvaraj1601@users.noreply.github.com>
2024-09-23 23:58:03 +0200

    Update label_checker.py

--- .github/workflows/label_checker.py
+++ .github/workflows/label_checker.py
@@ -6,24 +6,31 @@ import requests
 api_url = "https://api.github.com/repos/{}/issues/{}"
 github_token = os.environ["GITHUB_TOKEN"]
 
+# Define the required labels
+required_labels = ["bug", "documentation", "enhancement"]
+
 # Get the issue details from the GitHub event
 issue_number = os.environ["GITHUB_EVENT_ISSUE_NUMBER"]
 issue_labels = json.loads(os.environ["GITHUB_EVENT_ISSUE_LABELS"])
 iteration_info = os.environ["GITHUB_EVENT_ITERATION"]
-print(f"iteration_info: {iteration_info}")
 
-# Define the required labels
-required_labels = ["bug", "documentation", "enhancement"]
+# Check if iteration information is captured
+if iteration_info is None:
+    flag_iteration = False
+else:
+    flag_iteration = True
 
 # Check if any of the required labels are present
-label_found = False
+flag_label = False
+print(f"Issue Labels: " {issue_labels})
 for label in required_labels:
     if label in [l["name"] for l in issue_labels]:
-        label_found = True
+        flag_label = True
         break
 
+stat_flag = flag_iteration & flag_label
 # If no required labels are found, reopen the issue
-if not label_found:
+if not stat_flag:
     print("Label Check: Failure. Reopening the issue.")
     headers = {
         "Authorization": f"token {github_token}",
